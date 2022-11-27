a78f6caef170b6e402ed7e4f9d9dd78aface9334
thyme08 <miftahakbar08@gmail.com>
2022-11-27 21:24:05 +0700

    awal

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -16,7 +16,7 @@ use yii\helpers\Url;
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <!-- <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
+                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">Admin</a>
@@ -59,7 +59,7 @@ use yii\helpers\Url;
                     //         ['label' => 'foto', 'url' => ['fotobackground/index'], 'iconStyle' => 'far'],
 
                     //     ]
-                    // ], 
+                    // ], y
                     [
                         'label' => 'About',
                         'icon' => 'th',
@@ -70,11 +70,10 @@ use yii\helpers\Url;
                     ],
                     // ['label' => 'produk', 'url' => ['produk/index'],'icon' => 'th', ],
 
-                    ['label' => 'produk', 'url' => ['project/index'],'icon' => 'th', ],
-                    ['label' => 'tugas proyek', 'url' => ['project/index'],'icon' => 'th', ],
-                    ['label' => 'foto tugas', 'url' => ['project/index'],'icon' => 'th', ],
-                    ['label' => 'riwayat status', 'url' => ['project/index'],'icon' => 'th', ],
-                    ['label' => 'riwayat status', 'url' => ['project/index'],'icon' => 'th', ],
+                    ['label' => 'dashboard', 'url' => ['project/index'],'icon' => 'th', ],
+                    ['label' => 'proyek', 'url' => ['project/index'],'icon' => 'th', ],
+                    ['label' => 'semua tugas', 'url' => ['project/index'],'icon' => 'th', ],
+                    ['label' => 'verifikasi', 'url' => ['project/index'],'icon' => 'th', ],
 
 
 
